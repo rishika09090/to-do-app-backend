2ed99d5dc7176ffe175ab39140e260681fbc6460
Rishika Kumari <rishika09090@gmail.com>
2025-03-02 20:55:57 +0530

    updated jwt

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import User from "../model/user.model.js";
 export const authenticate = async (req, res, next) => {
-  const token = req.cookies.jwt;
+  const token = req.cookies.jwt || req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
